be6c000f7a66766e99ffccd983e07f5116b24e91
Denisecl1 <denisecamp21@gmail.com>
2026-04-30 13:06:56 -0600

    ...

--- js/app.js
+++ js/app.js
@@ -2,37 +2,46 @@ let currentIndex = 1;
 let carouselInstance;
 
 function mostrarSideBySide() {
+  // Mostramos contenedor SBS y sus etiquetas
   document.getElementById("mainViewer").classList.remove("d-none");
   document.getElementById("sbsLabels").classList.remove("d-none");
+  
+  // Ocultamos el visor Anaglifo
   document.getElementById("anaglifoViewer").classList.add("d-none");
 
-  // Reanudar carrusel
+  // Reanudamos el carrusel para el recorrido normal
   carouselInstance.cycle();
 }
 
 function mostrarAnaglifo() {
+  // Ocultamos SBS y sus etiquetas
   document.getElementById("mainViewer").classList.add("d-none");
   document.getElementById("sbsLabels").classList.add("d-none");
+  
+  // Mostramos el visor Anaglifo
   document.getElementById("anaglifoViewer").classList.remove("d-none");
 
-  // Pausar carrusel
+  // Pausamos carrusel para evitar mareos al usar lentes 3D
   carouselInstance.pause();
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  const carousel = document.getElementById("stereoCarousel");
+  const carouselElement = document.getElementById("stereoCarousel");
   const mainViewer = document.getElementById("mainViewer");
   const anaglifoImage = document.getElementById("anaglifoImage");
 
-  // Instancia del carrusel Bootstrap
-  carouselInstance = new bootstrap.Carousel(carousel, {
-    interval: 3000,   // cambia cada 4 segundos
+  // Instancia del carrusel de Bootstrap
+  carouselInstance = new bootstrap.Carousel(carouselElement, {
+    interval: 3000,   // Tiempo de transición automático (3 segundos)
     ride: "carousel"
   });
 
-  carousel.addEventListener("slid.bs.carousel", function (event) {
+  // Evento que escucha cada vez que la imagen del carrusel cambia
+  carouselElement.addEventListener("slid.bs.carousel", function (event) {
+    // event.to empieza en 0, sumamos 1 para igualar el nombre del archivo (1 al 15)
     currentIndex = event.to + 1;
 
+    // Actualización dinámica de las rutas de las imágenes en los visores inferiores
     mainViewer.src = `img/SBS${currentIndex}.JPG`;
     anaglifoImage.src = `img/ana${currentIndex}.JPG`;
   });
