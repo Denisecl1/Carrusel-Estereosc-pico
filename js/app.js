df7a203116e43aa523cdf3e0ee8cca43421046a4
Denisecl1 <denisecamp21@gmail.com>
2026-04-30 11:41:30 -0600

    Agregamos las 15 imagenes tanto SBS y anaglifo y agregamos mejoras para visualizarlas

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+let currentIndex = 1;
+
 function mostrarSideBySide() {
   document.getElementById("mainViewer").classList.remove("d-none");
   document.getElementById("sbsLabels").classList.remove("d-none");
@@ -9,3 +11,16 @@ function mostrarAnaglifo() {
   document.getElementById("sbsLabels").classList.add("d-none");
   document.getElementById("anaglifoViewer").classList.remove("d-none");
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const carousel = document.getElementById("stereoCarousel");
+  const mainViewer = document.getElementById("mainViewer");
+  const anaglifoImage = document.getElementById("anaglifoImage");
+
+  carousel.addEventListener("slid.bs.carousel", function (event) {
+    currentIndex = event.to + 1;
+
+    mainViewer.src = `img/SBS${currentIndex}.jpg`;
+    anaglifoImage.src = `img/ana${currentIndex}.jpg`;
+  });
+});
