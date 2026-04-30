1edd5022805c402021fe9d83fcddaf928e32f0d4
Denisecl1 <denisecamp21@gmail.com>
2026-04-30 12:24:51 -0600

    Mejoramos el JS y agregamos la imagen para el favicon

--- js/app.js
+++ js/app.js
@@ -1,21 +1,34 @@
 let currentIndex = 1;
+let carouselInstance;
 
 function mostrarSideBySide() {
   document.getElementById("mainViewer").classList.remove("d-none");
   document.getElementById("sbsLabels").classList.remove("d-none");
   document.getElementById("anaglifoViewer").classList.add("d-none");
+
+  // Reanudar carrusel
+  carouselInstance.cycle();
 }
 
 function mostrarAnaglifo() {
   document.getElementById("mainViewer").classList.add("d-none");
   document.getElementById("sbsLabels").classList.add("d-none");
   document.getElementById("anaglifoViewer").classList.remove("d-none");
+
+  // Pausar carrusel
+  carouselInstance.pause();
 }
 
 document.addEventListener("DOMContentLoaded", function () {
   const carousel = document.getElementById("stereoCarousel");
   const mainViewer = document.getElementById("mainViewer");
   const anaglifoImage = document.getElementById("anaglifoImage");
+
+  // Instancia del carrusel Bootstrap
+  carouselInstance = new bootstrap.Carousel(carousel, {
+    interval: 3000,   // cambia cada 4 segundos
+    ride: "carousel"
+  });
 
   carousel.addEventListener("slid.bs.carousel", function (event) {
     currentIndex = event.to + 1;
